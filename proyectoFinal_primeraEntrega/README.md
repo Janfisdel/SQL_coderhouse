43222745e34d3734543bbefcd261751cc0f6b49e
Jana Fisdel <janafisdel@gmail.com>
2022-11-22 21:11:05 -0300

    first commit

---
+++ proyectoFinal_primeraEntrega/README.md
@@ -0,0 +1,23 @@
+# CURSO SQL - CODER HOUSE
+
+## Comisión 34970
+
+### Situación real
+- En mi trabajo contamos de forma física con distintos freezers con muestras de sangre desde hace más de 20 años de un estudio en particular.
+- Cada paciente tiene un número identificatorio único.
+- Cada tramo del estudio se divide en varias secciones.
+- cada tramo cada paciente (o la mayoría de ellos) tiene una o dos muestras.
+- Dentro de cada freezer las muestras están guardadas en cajas. Cada caja tiene algún tramo del estudio, pero cada tramo lo conforman varias cajas poque no entran en una sola. También se les puso número a las cajas en el orden de las muestras que tiene dentro.
+- Las cajas de un mismo tramo no están guardadas juntas ni en orden, ni siquiera en el mismo freezer.
+
+### Proyecto Base de datos SQL de muestras de sangre
+- Mi idea para el proyecto es digitalizar la información de las muestras de sangre.
+- Dentro de cada tramo del estudio se repiten los pacientes, por lo que estos se pueden separar en una tabla de pacientes únicos.
+- Cada caja tiene una sección del tramo. Varias cajas son parte de un mismo tramo.
+- También se repiten los freezers donde están guardadas las muestras, ya que un solo freezer guarda muchas cajas con muchas muestras dentro.
+- Con mi proyecto busco que al necesitar encontrar una muestra física, dentro de las miles que tenemos, podamos encontrar rápidamente donde está localizada.
+
+### _Contenido del repositorio_
+| # | Clase | Contenido de la entrega | 
+| -------- | ---------| ---------|
+|1| Primera entrega Proyecto Final | Script de creacion de tablas y Diagrama entidad relacion en formato MWB y JPG
---
+++ proyectoFinal_primeraEntrega/desafio_FISDEL.sql
@@ -0,0 +1,76 @@
+-- El proyecto es un banco de muestras de sangre de un estudio para la deteccion de la enfermedad de Chagas
+drop schema if exists proyecto_sangre;
+create schema if not exists proyecto_sangre;
+use proyecto_sangre;
+
+-- La primera tabla incluye el listado de todos los pacientes en el tratamiento con sus datos personales y un numero que se le agregó para identificarlos
+drop table if exists pacientes;
+create table if not exists PACIENTES(
+    id_paciente varchar(4) not null,
+    nombre_paciente varchar(50) not null,
+    dni int not null,
+    fecha_nacimiento date not null,
+    provincia varchar (50),
+    primary key(id_paciente)
+);
+
+-- Esta tabla indica la cuantos freezers se posee y su ubicacion. 
+drop table if exists freezers;
+create table if not exists FREEZERS(
+    id_freezer int not null primary key,
+    nombre_freezer varchar(10),
+    ubicacion_edificio varchar(60)
+);
+
+-- Esta tabla indica cuantas cajas hay, en que freezer se guardan y cuantas muestras de sangre hay dentro de cada caja
+drop table if exists cajas_muestras;
+create table if not exists CAJAS_MUESTRAS(
+    id_caja int not null auto_increment,
+    nombre_caja varchar(10),
+    ubicacion_freezer int not null,
+    cantidad_muestras int not null,
+    primary key(id_caja),
+    CONSTRAINT `fk_cajas` foreign key(ubicacion_freezer) references freezers(id_freezer)
+     
+);
+
+-- Todas las tablas siguientes son similares. Cada una representa un tramo del estudio en el que se tomó muestras de casi todos los pacientes. 
+-- Cada tramo incluye muchas muestas de sangre que estan guardadas en difentes cajas ( de la tabla anterior). 
+-- En este caso ejemplifiqué con 3 tablas, para 3 tramos pero hay mas de 20 tramos en total.
+
+drop table if exists tramo_S0;
+create table if not exists tramo_S0(
+    id_muestraS0 int not null auto_increment,
+    paciente varchar(4) not null,
+    protocolo int not null,
+    ubicacion_caja int not null,
+    fecha_extraccion date,
+    primary key (id_muestraS0, protocolo),
+    CONSTRAINT `fk_muestraS0` foreign key(ubicacion_caja) references cajas_muestras(id_caja),
+    CONSTRAINT `fk_pacientesS0` foreign key(paciente) references pacientes(id_paciente)
+);
+
+drop table if exists tramo_S1;
+create table if not exists tramo_S1(
+    id_muestraS1 int not null auto_increment,
+    paciente varchar(4) not null,
+    protocolo int not null,
+    ubicacion_caja int not null,
+    fecha_extraccion date,
+    primary key (id_muestraS1, protocolo),
+    CONSTRAINT `fk_muestraS1` foreign key(ubicacion_caja) references cajas_muestras(id_caja),
+    CONSTRAINT `fk_pacientesS1` foreign key(paciente) references pacientes(id_paciente)
+);
+
+drop table if exists tramo_S2;
+create table if not exists tramo_S2(
+    id_muestraS2 int not null auto_increment,
+    paciente varchar(4) not null,
+    protocolo int not null,
+    ubicacion_caja int not null,
+    fecha_extraccion date,
+    primary key (id_muestraS2, protocolo),
+    CONSTRAINT `fk_muestraS2` foreign key(ubicacion_caja) references cajas_muestras(id_caja),
+    CONSTRAINT `fk_pacientesS2` foreign key(paciente) references pacientes(id_paciente)
+);
+
